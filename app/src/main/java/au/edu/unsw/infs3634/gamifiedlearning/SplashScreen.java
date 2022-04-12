a1bba4fa7a570eb7586060f9d52f0ba027729993
Nevinliu <z5267146@ad.unsw.edu.au>
2022-04-12 15:45:07 +1000

    Added splash screen

---
+++ app/src/main/java/au/edu/unsw/infs3634/gamifiedlearning/SplashScreen.java
@@ -0,0 +1,33 @@
+package au.edu.unsw.infs3634.gamifiedlearning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        }, 2000);
+
+    }
+
+    public void openMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
